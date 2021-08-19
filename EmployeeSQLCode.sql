e49db37aefb1621e56ecb22ff6b2ee856530f8d9
WayneJ2 <84686128+WayneJ2@users.noreply.github.com>
2021-08-18 18:37:15 -0700

    Starting Code
    starting SQL code and sample relationships in access

---
+++ EmployeeSQLCode.sql
@@ -0,0 +1,30 @@
+-- One to one
+
+
+
+
+
+--One to Many
+
+
+
+
+
+
+
+
+
+CREATE TABLE Employees (
+    id SERIAL,
+    emp_no VARCHAR,
+    emp_title_id VARCHAR,
+    birthdate ,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex VARCHAR,
+    hire_date,
+);
+
+CREATE TABLE Departments(
+    dept
+)
